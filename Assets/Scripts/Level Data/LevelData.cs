adaec6c0da5d2607215bc0f70d6bc8ba0a8c6c27
AmulSingh11 <105587383+AmulSingh11@users.noreply.github.com>
2024-08-29 13:31:50 +0530

    new level

--- Assets/Scripts/Level Data/LevelData.cs
+++ Assets/Scripts/Level Data/LevelData.cs
@@ -6,6 +6,7 @@ public class LevelData : ScriptableObject
     public Texture[] cylinderTextures;
     public Vector3[] initialRotations;
     public Vector3[] correctRotations;
+    public Vector3[] correctPlacements;
     public int[] groupIds; // Array to specify group IDs for each cylinder (-1 for no group)
     public float timeLimit;
     public string Question;
